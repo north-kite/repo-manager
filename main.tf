d56880a22e963bbe005358b1c6c070a6589221a0
Daniel Hill <dan@mamu.co>
2021-10-03 22:03:13 +0100

    manage existing concourse and manage-mysql-user repos

--- main.tf
+++ main.tf
@@ -1,5 +1,17 @@
-module "repo-manager" {
+module "repo_manager" {
   source      = "./github_repo"
   name        = "repo-manager"
   description = "Manage repo configuration in this org"
+}
+
+module "terraform_aws_concourse" {
+  source      = "./github_repo"
+  name        = "terraform-aws-concourse"
+  description = "Terraform module for deploying Concourse onto AWS EC2"
+}
+
+module "manage_mysql_user" {
+  source      = "./github_repo"
+  name        = "manage-mysql-user"
+  description = "Manages MySQL users"
 }
